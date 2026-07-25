84097add39229db7e465845c37fd23db1b039a92
alrizetesilva <alrizete.silva36@gmail.com>
2026-07-24 21:36:37 -0300

    aula dia 24/07.

---
+++ Atividades/LISTA_ATVIDADES_01/programa_01-01/main.py
@@ -0,0 +1,41 @@
+peso = float(input("informe seu peso:"))
+altura = float(input("informe sua altura:"))
+
+IMC = peso/(altura*altura)
+print(IMC)
+if imc > 18.5:
+   print("você esta mal seu imc é:")
+if imc  <18.5:
+   print("você esta bem seu imc é:") 
+if imc >25.0 <29.9:
+  print("você esta obeso classe 1 seu imc è:")   
+if  imc >30.0 <39.9:
+  print("você esta obeso classe 2 seu imc é:")
+if imc >40.0:
+  print("você está obeso classe 3 seu imc é:")
+
+    
+   
+
+
+
+
+  imc <24.9:
+if peso normal
+    imc >25.0 imc <29.9:
+    if excesso de peso:
+imc >30.0 imc <34.9:
+if obesidade classe 1:
+imc >35.0 imc <39.9:
+if obesidade classe 2
+imc >40.0 = 40.0 
+if obesidade classe 3
+
+    
+
+
+
+
+
+
+
