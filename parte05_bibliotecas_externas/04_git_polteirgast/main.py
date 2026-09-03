84aecbac14b3768d65c58e6610b83e1c854b75af
alrizetesilva36-droid/desenvolvedorphython_qua.544.003 <alrizete.silva36@gmail.com>
2026-09-02 21:38:32 -0300

    02/09/2026

---
+++ parte05_bibliotecas_externas/04_git_polteirgast/main.py
@@ -0,0 +1,26 @@
+import pyautogui as auto
+from datetime import date
+
+
+def hoje():
+    return date.today().strftime("%d/%m/%Y")
+
+def main():
+    auto.PAUSE = 1
+
+    auto.press("win")
+    auto.write("cmd")
+    auto.press("enter")
+    auto.write("cd c:/Users/ALUNO/Alrizete/desenvolvedorphython_qua.544.003")
+    auto.press("enter")
+    auto.write("git add .")
+    auto.press("enter")
+    auto.write(f'git commit -m "{hoje()}"')
+    auto.press("enter")
+    auto.write("git push")
+    auto.press("enter")
+
+
+
+if __name__ == "__main__":
+    main()
