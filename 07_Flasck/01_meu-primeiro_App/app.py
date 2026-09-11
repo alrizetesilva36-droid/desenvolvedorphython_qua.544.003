17c24d5b7f088a5cc5befd8b519269b47e9b94d4
alrizetesilva36-droid/desenvolvedorphython_qua.544.003 <alrizete.silva36@gmail.com>
2026-09-10 21:52:44 -0300

    10/09/2026

---
+++ 07_Flasck/01_meu-primeiro_App/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET","POST"])
+def index():
+    if request.method == "POST": 
+        nome = None
+    return render_template("index.html", nome=nome)
+
+@app.route("/novaPagina")
+def nova_pagina():
+    return render_template("segunda-pagina.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
